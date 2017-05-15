850c96d4b4058bc842afa8dad2751f2a77d496e4
akshayAppd <akshay.srivastava@appdynamics.com>
2017-05-15 16:34:19 +0530

    AWS-KMS Monitoring Extension First Commit

---
+++ src/main/java/com/appdynamics/extensions/aws/kms/AWSKMSMetricsProcessor.java
@@ -0,0 +1,55 @@
+package com.appdynamics.extensions.aws.kms;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.model.Metric;
+import com.appdynamics.extensions.aws.config.MetricType;
+import com.appdynamics.extensions.aws.metric.NamespaceMetricStatistics;
+import com.appdynamics.extensions.aws.metric.StatisticType;
+import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
+import com.appdynamics.extensions.aws.metric.processors.MetricsProcessorHelper;
+
+/**
+ * Created by akshay.srivastava on 10/05/17.
+ */
+public class AWSKMSMetricsProcessor implements MetricsProcessor {
+
+    private static final String NAMESPACE = "AWS/KMS";
+
+    private List<MetricType> metricTypes;
+
+    private Pattern excludeMetricsPattern;
+
+    public AWSKMSMetricsProcessor(List<MetricType> metricTypes,
+                                  Set<String> excludeMetrics) {
+        this.metricTypes = metricTypes;
+        this.excludeMetricsPattern = MetricsProcessorHelper.createPattern(excludeMetrics);
+    }
+
+    public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch) {
+        return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch,
+                NAMESPACE,
+                excludeMetricsPattern);
+    }
+
+    public StatisticType getStatisticType(Metric metric) {
+        return MetricsProcessorHelper.getStatisticType(metric, metricTypes);
+    }
+
+    public Map<String, Double> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
+        Map<String, String> dimensionToMetricPathNameDictionary = new HashMap<String, String>();
+
+        return MetricsProcessorHelper.createMetricStatsMapForUpload(namespaceMetricStats,
+                dimensionToMetricPathNameDictionary, false);
+    }
+
+    public String getNamespace() {
+        return NAMESPACE;
+    }
+
+}
