510d13b2c2bcbaf1e3a92702f91ac7a2576f11fd
akshayAppd <akshay.srivastava@appdynamics.com>
2020-05-15 13:29:31 +0530

    Upgrade to aws commons 2.2.3.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# AWS KMS Monitoring Extension
+
+## 1.1.0
+1. Updated the aws-cloudwatch-exts-commons to 2.2.3.1 from 1.0.1
--- src/test/java/com/appdynamics/extensions/aws/dynamodb/AWSKMSMonitorITest.java
+++ src/test/java/com/appdynamics/extensions/aws/dynamodb/AWSKMSMonitorITest.java
@@ -1,3 +1,11 @@
+/*
+ *   Copyright 2018. AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
 package com.appdynamics.extensions.aws.dynamodb;
 
 import static org.junit.Assert.assertTrue;
@@ -17,7 +25,7 @@ public class AWSKMSMonitorITest {
     @Test
     public void testMetricsCollectionCredentialsEncrypted() throws Exception {
         Map<String, String> args = Maps.newHashMap();
-        args.put("config-file", "src/test/resources/conf/itest-encrypted-config.yaml");
+        args.put("config-file", "src/test/resources/conf/itest-encrypted-config.yml");
 
         TaskOutput result = classUnderTest.execute(args, null);
         assertTrue(result.getStatusMessage().contains("successfully completed"));
@@ -26,7 +34,7 @@ public class AWSKMSMonitorITest {
     @Test
     public void testMetricsCollectionWithProxy() throws Exception {
         Map<String, String> args = Maps.newHashMap();
-        args.put("config-file", "src/test/resources/conf/itest-proxy-config.yaml");
+        args.put("config-file", "src/test/resources/conf/itest-proxy-config.yml");
 
         TaskOutput result = classUnderTest.execute(args, null);
         assertTrue(result.getStatusMessage().contains("successfully completed"));
